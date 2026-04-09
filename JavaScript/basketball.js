b7a32642de73a92a9376457733ef1f0e75bde2ba
austintheemperor <dirtbikepro118@gmail.com>
2026-04-09 08:49:08 -0500

    making the basketball game

---
+++ JavaScript/basketball.js
@@ -0,0 +1,58 @@
+var team1Score = 0
+var team2Score = 0
+var turn = "team1"
+
+function roll() {
+    let result = ""
+    var randomNum = Math.floor(Math.random() * 10 + 1)
+
+    if (randomNum > 8) {
+        if (turn == "team1") {
+            team1Score += 3
+            result = "Team 1 Scored a 3 pointer"
+        } else if (turn == "team2") {
+            team2Score += 3
+            result = "Team 2 Scored a 3 pointer"
+        }
+    } else if (randomNum > 5) {
+        if (turn == "team1") {
+            team1Score += 2
+            result = "Team 1 Scored 2 points"
+        } else if (turn == "team2") {
+            team2Score += 2
+            result = "Team 2 Scored 2 points"
+        }
+    } else if (randomNum > 3) {
+        result = "Miss!"
+    } else if (randomNum > 0) {
+        if (turn == "team1") {
+            turn = "team2"
+            result = "Team 1 turned the ball over to team 2"
+        } else if (turn == "team2") {
+            turn = "team1"
+            result = "Team 2 turned the ball over to team 1"
+        }
+
+    }
+    let resultText = ""
+    if (team1Score >= 30 && team2Score >= 30) {
+    resultText = "Tie!"
+    } else if (team1Score >= 30) {
+    resultText = "Team 1 Wins!"
+    } else if (team2Score >= 30) {
+    resultText = "Team 2 Wins!"
+    }
+
+    document.getElementById('game').innerHTML = `
+        <h2>Basket Ball Simulator</h2>
+        <p class="result-text">Team 1 Score: ${team1Score}</p>
+        <p class="result-text">Team 2 Score: ${team2Score}</p>
+        <p class="result-text">${result}</p>
+
+        ${resultText ? `<h3>${resultText}</h3>` : ""}
+
+        <button class="roll-btn" onclick="roll()">Roll</button>
+        <button class="roll-btn" onclick="window.location.href='Index.html';">Home</button>
+    `;
+}
+roll()
